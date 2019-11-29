7cb3f9bafff0d76fd17adbe21187fdbbe2a14a8c
magga98 <42964951+magga98@users.noreply.github.com>
2019-11-29 23:09:20 +0000

    fda

--- src/buttons.js
+++ src/buttons.js
@@ -1,17 +1,17 @@
-filterSelection('all');
+filterSelection("none");
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("box");
-  if (c == "all") c = "";
+  if (c == "none") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
 // Show filtered elements
-function AddClass(element, name) {
+function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -23,7 +23,7 @@ function AddClass(element, name) {
 }
 
 // Hide elements that are not selected
-function RemoveClass(element, name) {
+function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
