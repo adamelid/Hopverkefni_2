e4adb70a5be973b6cb0d7f53fe5fff8550e6a791
adamelid <adamelid16@gmail.com>
2019-11-30 23:06:06 +0000

    Update buttons.js

--- src/buttons.js
+++ src/buttons.js
@@ -1,24 +1,33 @@
 function html(){
   var x = document.getElementsByClassName("box-html");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }else {
-    x.style.display = "none";
+  var i = 0, l = x.length;
+  for( ; i < l; i++) {
+    if (x[i].style.display === "none") {
+      x[i].style.display = "block";
+    }else {
+      x[i].style.display = "none";
+    }
   }
 }
 function css(){
   var x = document.getElementsByClassName("box-css");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }else {
-    x.style.display = "none";
+  var i = 0, l = x.length;
+  for( ; i < l; i++) {
+    if (x[i].style.display === "none") {
+      x[i].style.display = "block";
+    }else {
+      x[i].style.display = "none";
+    }
   }
 }
 function java(){
-  var x = document.getElementsByClassName("box-java");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }else {
-    x.style.display = "none";
+  var x = document.getElementsByClassName("box-javascript");
+  var i = 0, l = x.length;
+  for( ; i < l; i++) {
+    if (x[i].style.display === "none") {
+      x[i].style.display = "block";
+    }else {
+      x[i].style.display = "none";
+    }
   }
 }
