433ef0030c8b1efc455c7c80e80f7cfdc92f0f72
magga98 <42964951+magga98@users.noreply.github.com>
2019-11-30 00:41:26 +0000

    update

--- src/buttons.js
+++ src/buttons.js
@@ -1,16 +1,15 @@
-filterSelection("all");
+//Reyndum að tengja kóðann okkar við svipað dæmi á netinu.
+filterSelection("all")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("box");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
 function AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -22,7 +21,6 @@ function AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
 function RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -35,8 +33,7 @@ function RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementsByClassName("buttons");
+var btnContainer = document.getElementById("buttons");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
